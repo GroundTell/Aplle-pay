69450dc5cf997fd33e02f3b3e897eb7e89d173dd
student <student@student-9.local>
2018-03-29 14:54:53 +0300

    Create aplication

--- Aplle pay/ViewController.swift
+++ Aplle pay/ViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var ImageView: UIImageView!
+    @IBOutlet weak var correctWorldlabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        sender.isEnabled = false
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
